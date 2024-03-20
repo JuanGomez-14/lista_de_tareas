78bbbf538fd7c11fe2ad3e12d7db91b41514395d
JuanMGomez14 <juang@eliteautomationgroup.com>
2024-03-19 22:45:25 -0500

    se actualiza taskcontroller para las actualizaciones de las tareas

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -1,4 +1,6 @@
-class TaskController {
+import 'package:flutter/material.dart';
+
+class TaskController extends ChangeNotifier {
   List<Map<String, dynamic>> tasks = [];
 
   void addTask(String title) {
@@ -6,13 +8,16 @@ class TaskController {
       'title': title,
       'completed': false,
     });
+    notifyListeners();
   }
 
   void removeTask(int index) {
     tasks.removeAt(index);
+    notifyListeners();
   }
 
   void toggleTaskCompletion(int index) {
     tasks[index]['completed'] = !tasks[index]['completed'];
+    notifyListeners();
   }
 }
