4483bd4219c2cfd1af423af9e1b3342e20c85d0c
JuanMGomez14 <juang@eliteautomationgroup.com>
2024-03-19 22:21:25 -0500

    Se actualiza main.dart para llamar un widget que es el de la aplicacion de tareas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lista_de_tareas/views/widgets/FormFields.dart';
+import 'package:lista_de_tareas/task_list_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,14 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lista de Tareas',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Lista de Tareas'),
-        ),
-        body: Center(
-          child: FormFieldsWidget(), // Aquí es donde agregas tu widget
-        ),
-      ),
+      home: TaskListScreen(),
     );
   }
 }
